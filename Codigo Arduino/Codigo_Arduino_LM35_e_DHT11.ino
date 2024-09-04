5cc0b4e40ddfe1670de275160e5ef56f3ede812d
Th-Sanchez <thiago.peluci@sptech.school>
2024-09-04 12:01:46 -0300

    Adição de comentários ao código do arduino

--- Codigo Arduino/Codigo_Arduino_LM35_e_DHT11.ino
+++ Codigo Arduino/Codigo_Arduino_LM35_e_DHT11.ino
@@ -1,33 +1,40 @@
-#include "DHT.h"
+#include "DHT.h" // Insere a biblioteca DHT.h no projeto
 
-#define TIPO_SENSOR DHT11
+#define TIPO_SENSOR DHT11 // Define o tipo de sensor que será utilizado
 
-const int PINO_SENSOR_DHT11 = A0;
-const int PINO_SENSOR_TEMPERATURA = A2; 
-float temperaturaCelsius;
+const int PINO_SENSOR_DHT11 = A0; // Define a entrada analógica que será utilizada pelo sensor DHT11
+const int PINO_SENSOR_TEMPERATURA = A2; // Define a entrada analógica que será utilizada pelo sensor LM35
+float temperaturaCelsius; // Criação de uma variavel que armazenará o valor da temperatura
 
-DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR);
+DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR); // Relaciona o tipo de sensor com a entrada que está sendo utilizada
 
+// Da um start no sensor para que comece a captar as informações
 void setup() {
   Serial.begin(9600);
   sensorDHT.begin();
 }
 
+// Loop de medição que se repetirá automaticamente
 void loop() {
-  int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
-  float umidade = sensorDHT.readHumidity();
-  temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01;
+  int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA); // Armazenamento do dado da entrada analógica em uma variável
+  float umidade = sensorDHT.readHumidity(); // Armaenamento do dado da entrada analógica em uma variável
+  temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01; // Calculo da temperatura
 
+  // Exibição do valor da temperatura 
   Serial.print("Temperatura: ");
   Serial.print(temperaturaCelsius);
   Serial.println(" ºC");
   
+  // Verificação se o valor recebido pelo sensor é realmente um dado possível de se ler
   if(isnan(umidade)){
+    // Exibição de uma mensagem de erro caso a leitura acabe não dando certo
     Serial.println("Umidade: Erro ao ler os dados do sensor");
   } else {
+    // Exibição da temperatura
     Serial.print("Umidade: ");
     Serial.print(umidade);
     Serial.println("%");
   }
+  // Adição de um delay entre cada amostragem
   delay(2000);
 }
